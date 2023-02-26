1573f308cbecca5c5a505d499c24d7808de2da18
ell-ska <ella@skagerstrom.se>
2023-02-26 19:50:59 +0100

    change target of link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../css/footer.css'
 export default () => {
     return (
         <footer className="footer">
-            <span className="footer__credits">Design by <a href="https://www.figma.com/community/file/947671983918628823">@riaarante</a></span>
+            <span className="footer__credits">Design by <a href="https://www.figma.com/community/file/947671983918628823" target="_blank">@riaarante</a></span>
         </footer>
     )
 }
